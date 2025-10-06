dfdd1b48978fa25bf70998e3b85d1215d3c2f8e0
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-10-06 22:16:58 +0600

    4563

---
+++ E_Max.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int maxx=0;
+    scanf("%d\n",&n);
+    for(int i=1;i<=n;i++)
+    {
+        int num;
+        scanf("%d ",&num);
+        {
+         if(num>maxx)
+         {
+           maxx=num;
+         }
+
+         
+        
+
+        }
+    }
+    printf("%d",maxx);
+
+}
