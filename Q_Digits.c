043697b5a0549139d7fe6456eeadab1234e50e2a
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-10-07 21:51:31 +0600

    80405

---
+++ Q_Digits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+
+      int num;
+    scanf("%d",&num);
+
+    do
+    {
+        printf("%d ",num%10);
+        num=num/10;
+
+    }
+    while(num!=0);
+    printf("\n");
+
+}
+}
